b5177a87f31dd989f0586e8ea0afa2331b9b0e1f
aysebetty <95138426+aysebetty@users.noreply.github.com>
2024-05-23 12:28:21 -0400

    Uyguladigimiz adimlar

--- src/test/java/team01_AlloverCommerceTestNG/pages/P1_HomePage.java
+++ src/test/java/team01_AlloverCommerceTestNG/pages/P1_HomePage.java
@@ -1,6 +1,41 @@
 package team01_AlloverCommerceTestNG.pages;
 
 public class P1_HomePage {
-int a = 4;
+
+/* Uyguladigimiz adimlar :
+A) Intellijeyde yaptigimiz degisiklikleri Github projemize eklemek icin
+1. git init (ilk defa olustururken)
+2. git branch ayse  (ayse isminde branch olusturma, mutlaka kendimize branch olusturup degisiklikleri bu branch uzerinden gondermeliyiz )
+3. git checkout ayse (main branchden kendi branchimize gecis)
+4. git branch  (hangi branchde oldugumuzu gosterir)
+gelen mesaj:
+* ayse
+  main   (* ayse branchinde oldugunu gosteriyor)
+5. git status (yine hangi branchde oldugumuzu gosteriyor)
+gelen mesaj:
+On branch ayse
+nothing to commit, working tree clean (commit edilen birsey olmadigini ve branchin bos oldugunu soyluyor)
+6. Degisikliklerimizi branche eklemek icin
+git add .
+git commit -m"aysebetul/23/5/24" (seklinde commitmize hem ismimizi hem tarihi yazabiliriz)
+7. kendi branchimizi githubtaki projemizin main branchine eklemek icin
+git push (yazariz bize " git push --set-upstream origin ayse " seklinde uyari verecektir gelen sekilde yazalim)
+8.git push --set-upstream origin ayse
+Gelen mesaj:
+Bu sekilde bize adres verir uzerine tiklayalim ve githuba ana branch'e merge istegi icin gidelim
+remote: Create a pull request for 'ayse' on GitHub by visiting:
+remote:      https://github.com/BinnurAG/AlloverCommerce.Team01-main/pull/new/ayse
+
+Bu linke gidince orda request isteginde bulunmaliyiz fakat kendimiz confirm etmeyelim sorumlular kabul edecek
+
+B) Githubtaki degisiklikleri localimize cekmek icin ise
+1. git checkout main  (main branche gecelim)
+2.git branch  (hangi branchde oldugumuzu seklinde kontrol edebiliriz)
+gelen mesaj:
+ayse
+*main   (* main branchinde oldugunu gosteriyor)
+3. git pull (main branchteki tum yeni hali localimize ceker)
+4.
+ */
 
 }
