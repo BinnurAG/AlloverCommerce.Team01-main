a69595cb0b47eeaf405afb37f8ddba25bce93b61
Bilsen Pehlivanoglu <bilsensaturnus@gmail.com>
2024-05-24 21:35:29 -0400

    us01

--- src/test/java/team01_AlloverCommerceTestNG/pages/P2_RegisterPage.java
+++ src/test/java/team01_AlloverCommerceTestNG/pages/P2_RegisterPage.java
@@ -1,8 +1,38 @@
 package team01_AlloverCommerceTestNG.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team01_AlloverCommerceTestNG.utilities.Driver;
 
 public class P2_RegisterPage {
+
+    public P2_RegisterPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[.='Register']")
+    public WebElement register;
+
+    @FindBy(id = "reg_username")
+    public WebElement userName;
+
+    @FindBy(id = "reg_email")
+    public WebElement emailAddress;
+
+    @FindBy(id = "reg_password")
+    public WebElement password;
+
+    @FindBy(id = "register-policy")
+    public WebElement privacyPolicy;
+
+    @FindBy(xpath = "//button[@name='register']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//*[.='Sign Out']")
+    public WebElement signOut;
+
+    @FindBy(xpath = "//*[.='Please enter a valid account username.']")
+    public WebElement resultMessage;
 
 }
