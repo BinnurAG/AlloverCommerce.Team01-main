08b63a3cbc8a02ebf1d4da384bdd7180b0aacf6f
Bilsen Pehlivanoglu <bilsensaturnus@gmail.com>
2024-05-28 15:29:52 -0400

    us01andus02

--- src/test/java/team01_AlloverCommerceTestNG/tests/us01Register/TC_02.java
+++ src/test/java/team01_AlloverCommerceTestNG/tests/us01Register/TC_02.java
@@ -1,42 +1,19 @@
-package team01_AlloverCommerceTestNG.tests;
+package team01_AlloverCommerceTestNG.tests.us01Register;
 
 import com.github.javafaker.Faker;
-import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import team01_AlloverCommerceTestNG.pages.P2_RegisterPage;
 import team01_AlloverCommerceTestNG.utilities.ConfigReader;
 import team01_AlloverCommerceTestNG.utilities.Driver;
 
-public class US01_Register_SignUp {
+public class TC_02 {
 
     P2_RegisterPage p2_registerPage = new P2_RegisterPage();
-
     Faker faker = new Faker();
 
-    @Test
-    public void testCase01() {
-//  Siteye müşteri olarak geçerli verilerle kayıt olunabilmeli(Register)
-//        1	Web sitesine git
-        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
-//        2	Register linkine tıkla
-       p2_registerPage.register.click();
-//        3	Username alanına bir veri gir
-//        4	Your Email address alanına bir veri gir
-        p2_registerPage.userName.sendKeys(faker.name().username(), Keys.TAB,
-                faker.internet().emailAddress(),Keys.TAB);
-//        5	Password alanına bir veri gir
-        p2_registerPage.password.sendKeys(ConfigReader.getProperty("registeredPassword"));
-        //       6	I agree to the privacy policy kontrol kutusunu seç
-        p2_registerPage.privacyPolicy.click();
-//        7	SIGN UP butonuna tıkla
-        p2_registerPage.submitButton.click();
-//        8	Anasayfanın açıldığını ve Sign Out linkinin göründüğünü doğrula
-        Assert.assertTrue(p2_registerPage.signOut.isDisplayed());
-        //Driver.closeDriver();
-    }
-    @Test
-    public void testCase02() { //FAIL EKRAN GORUNTUSU ALMALISIN
+    @Test(description = "US01 - TC02 The user should be able to register to the site with special characters")
+    public void specialCharacterTest() { //FAIL EKRAN GORUNTUSU ALMALISIN
 //  Siteye müşteri olarak kayıt olurken
 //  username alanına küçük harf,büyük harf,rakam ve özel karakterler girilebilmeli
 //        1	Web sitesine git
